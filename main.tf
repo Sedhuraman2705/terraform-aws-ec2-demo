183beecdc2ffcfeeaa9269ed3aeecf70c4fe75e4
Sedhuraman2705 <sedhuraman9040@gmail.com>
2026-02-26 12:28:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,9 +18,24 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+provider "aws" {
+  region = "us-east-1"   # change if needed
+}
+
+# Get latest Amazon Linux 2023 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-x86_64"]
+  }
+}
+
 # Create EC2 instance
 resource "aws_instance" "demo" {
-  ami = "ami-002b032d8520decf5"
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
   tags = {
